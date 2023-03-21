d3e13f7e099202673d55ce0fdfb0a56a6bc0afa0
imammashduqi <imammashduqi@students.amikom.ac.id>
2023-03-21 10:04:14 +0700

    first commit

---
+++ ClassTaxiDriver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxiDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            TaxiClass taxi = new TaxiClass();
+
+            // pengertian nilai properties
+            taxi.DriverName = "Pak imam";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            // pemanggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
